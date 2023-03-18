61449b0d744b4763bd25fac750910e61c3c6103c
ArtursMelmanis <124924639+ArtursMelmanis@users.noreply.github.com>
2023-03-18 19:43:54 +0200

    Update build_heap.py

--- build_heap.py
+++ build_heap.py
@@ -14,22 +14,22 @@ def sift_down(i, data, swaps):
         data[i], data[index] = data[index], data[i]
         sift_down(index, data, swaps)
 
-def build_heap(data):
+def build_heap(n, data):
     swaps = []
     n = len(data)
     for i in range(n // 2, -1, -1):
         sift_down(i, data, swaps)
-    return swaps
+    return len(swaps), swaps
 
 def main():
     n = int(input())
     data = list(map(int, input().split()))
     assert len(data) == n
 
-    swaps = build_heap(data)
-    print(len(swaps))
-    for swap in swaps:
-        print(swap[0], swap[1])
+    swap, swaps = build_heap(n, data)
+    print(swap)
+    for sw in swaps:
+        print(sw[0], sw[1])
 
 if __name__ == "__main__":
     main()
