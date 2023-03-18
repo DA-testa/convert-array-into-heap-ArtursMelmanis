eb3fc580a1e0afa0ea28778bf51833dfe472a23e
ArtursMelmanis <124924639+ArtursMelmanis@users.noreply.github.com>
2023-03-18 19:36:22 +0200

    Update build_heap.py

--- build_heap.py
+++ build_heap.py
@@ -1,6 +1,7 @@
 # python3
 
-def sift_down(n, data, i, swap, swaps):
+def sift_down(i, data, swaps):
+    n = len(data)
     index = i
     l = 2 * i + 1
     if l < n and data[l] < data[index]:
@@ -9,29 +10,26 @@ def sift_down(n, data, i, swap, swaps):
     if r < n and data[r] < data[index]:
         index = r
     if i != index:
-        data[i], data[index] = data[index], data[i]
-        swap += 1
         swaps.append((i, index))
-        swap, swaps = sift_down(n, data, index, swap, swaps)
-    return swap, swaps
+        data[i], data[index] = data[index], data[i]
+        sift_down(index, swap, swaps)
 
-def build_heap(n, data):
-    swap = 0
+def build_heap(data):
     swaps = []
+    n = len(data)
     for i in range(n // 2, -1, -1):
-        swap, swaps = sift_down(n, data, i, swap, swaps)
-    return swap, swaps
+        sift_down(i, data, swaps)
+    return swaps
 
 def main():
-    n = int(input().strip())
+    n = int(input())
     data = list(map(int, input().split()))
     assert len(data) == n
 
-    swap, swaps = build_heap(n, data)
-    
-    print(swap)
-    for sw in swaps:
-        print(sw[0], sw[1])
+    swaps = build_heap(data)
+    print(len(swaps))
+    for swap in swaps:
+        print(swap[0], swap[1])
 
 if __name__ == "__main__":
     main()
