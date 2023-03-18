bf366d0a39586b94d41fc78ec635521d28838961
ArtursMelmanis <124924639+ArtursMelmanis@users.noreply.github.com>
2023-03-18 19:22:53 +0200

    Update build_heap.py

--- build_heap.py
+++ build_heap.py
@@ -1,34 +1,26 @@
 # python3
 
-from math import floor
-
-def build_heap(n, data):
-    swap = 0
-    swaps = []
-    size = n - 1
-    for i in range(floor(n/2), -1, -1):
-       swap, swaps, data = sift_down(i, size, data, swap, swaps)
-    return swap, swaps
-    
-def sift_down(i, size, data, swap, swaps):
+def sift_down(n, data, i, swap, swaps):
     index = i
-    l = LeftChild(i + 1)
-    if l <= size and data[l] < data[index]:
+    l = 2 * i + 1
+    if l < n and data[l] < data[index]:
         index = l
-    r = RightChild(i + 1)
-    if r <= size and data[r] < data[index]:
+    r = 2 * i + 2
+    if r < n and data[r] < data[index]:
         index = r
     if i != index:
         data[i], data[index] = data[index], data[i]
         swap += 1
         swaps.append((i, index))
-        swap, swaps, data = sift_down(index, size, data, swap, swaps)
-    return swap, swaps, data
+        swap, swaps = sift_down(n, data, index, swap, swaps)
+    return swap, swaps
 
-def LeftChild(i):
-    return 2 * i - 1
-def RightChild(i):
-    return 2 * i
+def build_heap(n, data):
+    swap = 0
+    swaps = []
+    for i in range(n // 2, -1, -1):
+        swap, swaps = sift_down(n, data, i, swap, swaps)
+    return swap, swaps
 
 def main():
     n = int(input())
