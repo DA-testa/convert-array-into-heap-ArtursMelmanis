fe3779f52ce47f2ffb76d67ac0f49c8e4f15344b
ArtursMelmanis <124924639+ArtursMelmanis@users.noreply.github.com>
2023-03-18 17:51:00 +0200

    Update build_heap.py

--- build_heap.py
+++ build_heap.py
@@ -1,29 +1,38 @@
 # python3
 
-from math import floor
-
 def build_heap(n, data):
     swap = 0
     swaps = []
     size = n - 1
-    for i in range(floor(n/2), -1, -1):
-        swap, swaps, data = sift_down(i, size, data, swap, swaps)  
+    for i in range((n // 2), -1, -1, -1):
+        k = i
+        v = data[k]
+        heap = False
+        while not heap and 2 * k + 1 <= size:
+            j = 2 * k + 1
+            if j < size and data[j] > data[j + 1]:
+                j += 1
+            if v <= data[j]:
+                heap = True
+            else:
+                data[k] = data[j]
+                swap += 1
+                swaps.append((k, j))
+                k = j
+            data[k] = v
+        
     return swap, swaps
-
-
+    
 def main():
-
+    
     n = int(input())
     data = list(map(int, input().split()))
-
     assert len(data) == n
-
-    swaps = build_heap(data)
-
-    print(len(swaps))
-    for i, j in swaps:
-        print(i, j)
-
+    swap, swaps = build_heap(n, data)
+    print(swap)
+    
+    for swap in swaps:
+        print(swap[0], swap[1])
 
 if __name__ == "__main__":
     main()
