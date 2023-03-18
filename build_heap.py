736bd231b9c3e38cb58f4943efdd9f153d2fb222
ArtursMelmanis <124924639+ArtursMelmanis@users.noreply.github.com>
2023-03-18 17:32:48 +0200

    Update build_heap.py

--- build_heap.py
+++ build_heap.py
@@ -1,38 +1,25 @@
 # python3
 
+from math import floor
 
-def build_heap(data):
+def build_heap(n, data):
+    swap = 0
     swaps = []
-    # TODO: Creat heap and heap sort
-    # try to achieve  O(n) and not O(n2)
-
-
-    return swaps
+    size = n - 1
+    for i in range(floor(n/2), -1, -1):
+        swap, swaps, data = sift_down(i, size, data, swap, swaps)  
+    return swap, swaps
 
 
 def main():
-    
-    # TODO : add input and corresponding checks
-    # add another input for I or F 
-    # first two tests are from keyboard, third test is from a file
 
-
-    # input from keyboard
     n = int(input())
     data = list(map(int, input().split()))
 
-    # checks if lenght of data is the same as the said lenght
     assert len(data) == n
 
-    # calls function to assess the data 
-    # and give back all swaps
     swaps = build_heap(data)
 
-    # TODO: output how many swaps were made, 
-    # this number should be less than 4n (less than 4*len(data))
-
-
-    # output all swaps
     print(len(swaps))
     for i, j in swaps:
         print(i, j)
